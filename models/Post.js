99f93f06ef458b9173a767885dbc2eac650d6f31
Satoshi Matsushima <>
2022-06-14 00:04:56 +0900

    25. 投稿用(Post.js)のデータスキーマを作成してみよう

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+const mongoose = reqire('mongoose')
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 200,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+
+  { timestamps: true },
+)
+
+module.export = mongoose.model('Post', PostSchema)
