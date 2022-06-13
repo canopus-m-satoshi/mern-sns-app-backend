116ec1845b43c5a365b6c153e27d02949900d955
Satoshi Matsushima <>
2022-06-13 23:07:07 +0900

    22. ユーザーをフォローするAPIを自作してみよう

--- routes/users.js
+++ routes/users.js
@@ -51,4 +51,39 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+// ユーザーのフォロー
+router.put('/:id/follow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id)
+      const currentUser = await User.findById(req.body.userId)
+
+      // フォロワーに自分がいなかったらフォローできる
+      if (!user.followers.include(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        })
+
+        await user.updateOne({
+          $push: {
+            followings: req.body.userId,
+          },
+        })
+
+        return res.status(200).json('フォローしました')
+      } else {
+        return res
+          .status(403)
+          .json('あなたはすでにこのユーザーをフォローしています')
+      }
+    } catch (err) {
+      return res.status(500).json(err)
+    }
+  } else {
+    return res.status(500).json('自分自身をフォローできません')
+  }
+})
+
 module.exports = router
